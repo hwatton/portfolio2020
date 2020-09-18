d168bb3e333bcd4880a811ffe3f0390c7e1da7b0
hwatton <harry.watton@hotmail.co.uk>
2020-09-18 08:13:43 +0100

    Wednesday Lunch color work

--- static/main_site/graphics.js
+++ static/main_site/graphics.js
@@ -1,12 +1,39 @@
 const svgC = d3.select(".container")
 .append("svg")
-.attr("height", 500)
-.attr("width", 500)
+.attr("height", 400)
+.attr("width", 600)
 
-for (i=0;i<5;i++) {
+const leftBarDiv = d3.select(".leftBar")
+.append("svg")
+.attr("height", 400)
+.attr("width", 20)
+.style("background-color", "pink")
+
+for (i=0;i<200;i++) {
+
   svgC.append("rect")
-  .attr("height", 20)
-  .attr("width", 20)
-  .attr("x", ()=> {return i*30})
-  .attr("y", 20)
+  .attr("id", ()=> {return "rect_" + i})
+  .attr("height", 400)
+  .attr("width", 3)
+  .attr("x", ()=> { return i*3})
+  .attr("y", 0)
+  .attr("fill", ()=> {
+    return d3.interpolateRainbow(Math.random())
+  })
+}
+
+flicker()
+
+function flicker() {
+  setTimeout( function() {
+let rnd = Math.floor(Math.random()*200)
+let rndText = "#rect_" + rnd
+  d3.select(rndText).transition()
+  .duration(1500)
+  .attr("fill", ()=> {
+    return d3.interpolateRainbow(Math.random())
+  })
+
+  requestAnimationFrame(flicker)}, 50)
+
 }
